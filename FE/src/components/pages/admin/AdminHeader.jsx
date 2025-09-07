b42c4f9f299388b42d343c1c31bc3fddcecf2db5
nawrasfaour <nawrasfaour2003@gmail.com>
2025-09-08 01:06:07 +0300

    edited admin home

--- FE/src/components/pages/admin/AdminHeader.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import styles from './adminHeader.module.css';
-
-// UNUSED ADMIN COMPONENT: stubbed on 2025-09-07. Original implementation removed to avoid JSX parsing.
-export default function AdminHeader() { return null; }
